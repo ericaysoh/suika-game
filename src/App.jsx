4e56882aee84dfdee2a640115d348a93cc5f7326
Erica Oh <ericaysoh@gmail.com>
2023-11-29 21:46:42 -0800

    added fruits

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,53 @@
 import { Bodies, Engine, Render, Runner, World } from 'matter-js';
-// import { useState } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import './App.css'
 
 function App() {
   const engine = Engine.create();
-  const render = Render.create({
-    engine,
-    element: document.body,
-    options: {
-      wireframes: false,
-      background: "#F7F4C8",
-      width: 420,
-      height: 850,
-    }
-  });
+  useEffect(() => {
 
-  const world = engine.world;
-  const leftWall = Bodies.rectangle(15, 395, 30, 790, {
-    isStatic: true,
-    render: { fillStyle: "#E6B143"}
-  });
+    const render = Render.create({
+      engine,
+      element: document.body,
+      options: {
+        wireframes: false,
+        background: "#F7F4C8",
+        width: 420,
+        height: 850,
+      }
+    });
+  
 
-  World.add(world, [leftWall])
+    const world = engine.world;
+    const leftWall = Bodies.rectangle(15, 395, 30, 790, {
+      isStatic: true,
+      render: { fillStyle: "#E6B143"}
+    });
+    const rightWall = Bodies.rectangle(605, 395, 30, 790, {
+      isStatic: true,
+      render: { fillStyle: "#E6B143"}
+    });
+    const ground = Bodies.rectangle(310, 820, 620, 60, {
+      isStatic: true,
+      render: { fillStyle: "#E6B143"}
+    });
+    const topLine = Bodies.rectangle(310, 150, 620, 2, {
+      isStatic: true,
+      render: { fillStyle: "#E6B143"}
+    });
 
-  // return (
-    // <>
-    Render.run(render)
-    Runner.run(engine)
-    // </>
-  // )
+    World.add(world, [leftWall, rightWall, ground, topLine])
+
+    // return (
+      // <>
+      Render.run(render)
+      Runner.run(engine)
+      // </>
+    // )
+    // return (
+    //   world
+    // )
+  })
 }
 
 export default App

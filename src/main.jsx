554b760b8d1192dd8dec8a08094967e6e0c1dc1f
Erica Oh <ericaysoh@gmail.com>
2023-12-03 23:21:51 -0800

    removed React.StrictMode - with comments

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   // <React.StrictMode>
+    // removing React.StrictMode prevents alert messages from rendering more than once because StrictMode renders components twice on development mode to detect errors in code
     <App />
   // </React.StrictMode>,
 )
